44218a4b090cc014b8b5c04ec40e6a4e4ff9cd40
Tombazats <45174394+Tombazats@users.noreply.github.com>
2021-02-18 12:57:44 +0100

    Begin

---
+++ app/src/main/java/fr/epsi/mapappli/RayonActivity.java
@@ -0,0 +1,10 @@
+package fr.epsi.mapappli;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RayonActivity extends AppCompatActivity implements View.OnClickListener {
+}
