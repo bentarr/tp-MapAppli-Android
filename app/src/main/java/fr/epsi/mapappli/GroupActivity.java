c49425833d79fcfd90c7e43f2b62ed8f35b73a0a
bentarr <benetierpaul@gmail.com>
2021-02-18 14:26:08 +0100

    Changement lancement page groupes

--- app/src/main/java/fr/epsi/mapappli/GroupActivity.java
+++ app/src/main/java/fr/epsi/mapappli/GroupActivity.java
@@ -1,6 +1,24 @@
 package fr.epsi.mapappli;
 
-public class GroupActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
 
+public class GroupActivity extends EpsiActivity {
+
+    public static void displayActivity(HomeActivity activity){
+        Intent intent = new Intent(activity,GroupActivity.class);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+
+        setTitle("Groupe d'étudiants");
+
+    }
 }
