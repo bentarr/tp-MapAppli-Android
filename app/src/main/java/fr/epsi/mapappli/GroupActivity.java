8986623706482a33e15e289261a3ed0690dcf064
bentarr <benetierpaul@gmail.com>
2021-03-20 17:04:01 +0100

    CASE BOUTTON JORDAN/TOM/PAUL ONE PAGE

--- app/src/main/java/fr/epsi/mapappli/GroupActivity.java
+++ app/src/main/java/fr/epsi/mapappli/GroupActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GroupActivity extends EpsiActivity {
+public class GroupActivity extends EpsiActivity implements View.OnClickListener {
 
 
     public static void displayActivity(HomeActivity activity) {
@@ -15,12 +15,41 @@ public class GroupActivity extends EpsiActivity {
     }
 
 
+    public Students Tom = new Students("photo1", "Tom Bazats", "tom.bazats@epsi.fr", "Groupe 2");
+    public Students Jordan = new Students("photo2", "Jordan Leston", "jordan.leston@epsi.fr", "Groupe 2");
+    public Students Paul = new Students("photo3", "Paul Bénétier", "paul.benetier@epsi.fr", "Groupe 2");
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
         setTitle("Groupe d'étudiants");
         showBack();
+
+        findViewById(R.id.bouttonTom).setOnClickListener(this);
+        findViewById(R.id.bouttonPaul).setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bouttonJordan:
+                getIntent().putExtra("Students", this.Jordan);
+                startActivity(getIntent());
+                break;
+
+            case R.id.bouttonTom:
+                getIntent().putExtra("Students", this.Tom);
+                startActivity(getIntent());
+                break;
+
+            case R.id.bouttonPaul:
+                getIntent().putExtra("Students", this.Paul);
+                startActivity(getIntent());
+                break;
+        }
     }
 
 }
