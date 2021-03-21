83c9dfa5e71b4d3b4b3003c6e60202621a3d5f27
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2021-03-21 21:32:29 +0100

    Changes from merge

--- app/src/main/java/fr/epsi/mapappli/ProductListActivity.java
+++ app/src/main/java/fr/epsi/mapappli/ProductListActivity.java
@@ -6,10 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
-public class ProductListActivity extends AppCompatActivity {
+public class ProductListActivity extends EpsiActivity {
     private ArrayList<Produit> produits;
 
-    public static void displayActivity(AppCompatActivity activity)
+    public static void displayActivity(EpsiActivity activity)
     {
         Intent intent = new Intent(activity, ProductListActivity.class);
     }
