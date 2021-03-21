d6642785e42945f5962936bff5aa0f14e8cc7fc0
Tombazats <45174394+Tombazats@users.noreply.github.com>
2021-03-21 21:14:56 +0100

    Classe Rayon, RayonActivity, WSCall

--- app/src/main/java/fr/epsi/mapappli/RayonActivity.java
+++ app/src/main/java/fr/epsi/mapappli/RayonActivity.java
@@ -1,10 +1,17 @@
 package fr.epsi.mapappli;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
-public class RayonActivity extends AppCompatActivity implements View.OnClickListener {
+public class RayonActivity extends MainActivity{
+    private ArrayList<Rayon> rayon;
+
+    public static void displayActivity(MainActivity activity, String name, String url){
+        Intent intent = new Intent(activity, RayonActivity.class);
+        intent.putExtra("name", name);
+        intent.putExtra("url", url);
+        activity.startActivity(intent);
+    }
+
 }
